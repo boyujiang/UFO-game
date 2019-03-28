3b7e00cb62ef360f6234d75dc63f21b6136a17e7
boyujiang <47674048+boyujiang@users.noreply.github.com>
2019-03-27 23:44:10 -0400

    create project

---
+++ UFO Game/Assets/TutorialInfo/Scripts/playercontroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2 (movehorizontal, movevertical);
+        rb2d.AddForce(movement * speed);
+    }
+}

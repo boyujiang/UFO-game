0e21a514cdc1418dae3df64b5320d5e1ffac03f7
Lawrence Chung <chungl1@mcmaster.ca>
2019-03-30 16:11:56 -0400

    Added new buttons and behavior

--- UFO Game/Assets/Scripts/ItemCount.cs
+++ UFO Game/Assets/Scripts/ItemCount.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShopIncrease : MonoBehaviour
+public class ItemCount : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

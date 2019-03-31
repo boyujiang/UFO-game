6ee4b035f94e8d543b44bfd042457c5683ba2d6b
Lawrence Chung <chungl1@mcmaster.ca>
2019-03-31 13:33:27 -0400

    Add GameState Contoller

---
+++ UFO Game/Assets/Scripts/GameStoreController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStoreController : MonoBehaviour
+{
+    //public Map<String, Integer> buyableObjectsMap;
+
+    public Text itemCount;
+    public GameObject purchaseButton;
+
+    public GameState gs;
+
+    int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = int.ParseInt(itemCount.text);
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    public void upgradePlayer(){
+
+        int healthIncrease = 10; //Can change value
+
+
+    }
+}

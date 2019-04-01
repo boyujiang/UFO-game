e680beaa8d0e01b2f5a925ffd3c5020e17895d19
Lawrence Chung <chungl1@mcmaster.ca>
2019-04-01 19:14:56 -0400

    Transtion from store to levelselection

--- UFO Game/Assets/Scripts/BackButton.cs
+++ UFO Game/Assets/Scripts/BackButton.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BackButton : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +14,12 @@ public class BackButton : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    // void Update()
+    // {
         
+    // }
+
+    public void goBack(){
+        SceneManager.LoadScene("LevelSelection");
     }
 }

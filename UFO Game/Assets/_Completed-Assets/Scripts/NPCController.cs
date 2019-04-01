e864224261c392fba8a540c7fff147d9becfc4b9
Tao Sophia <taos1@mcmaster.ca>
2019-04-01 18:38:34 -0400

    Add projectile creation

--- UFO Game/Assets/_Completed-Assets/Scripts/NPCController.cs
+++ UFO Game/Assets/_Completed-Assets/Scripts/NPCController.cs
@@ -5,37 +5,49 @@ using UnityEngine;
 public class NPCController : MonoBehaviour
 {
     private float latestDirectionChangeTime;
-    private readonly float directionChangeTime = 3f;
+    private float directionChangeTime = 100f;
     private float characterVelocity = 2f;
     private Vector2 movementDirection;
     private Vector2 movementPerSecond;
+    private Rigidbody2D rb2d;
     private float spriteHeight = 256;
     private float spriteWidth = 256;
-    private float width = 2048;
-    private float height = 2048;
- 
- 
-    void Start(){
+    private float width = 400;
+    private float height = 400;
+
+
+    public void Start()
+    {
         latestDirectionChangeTime = 0f;
+        rb2d = GetComponent<Rigidbody2D>();
         calcuateNewMovementVector();
     }
- 
-    void calcuateNewMovementVector(){
+
+    void calcuateNewMovementVector()
+    {
         //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
         movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
         movementPerSecond = movementDirection * characterVelocity;
     }
- 
-    void Update(){
+
+
+
+    public void Update()
+    {
         //if the changeTime was reached, calculate a new movement vector
-        if (Time.time - latestDirectionChangeTime > directionChangeTime){
-             latestDirectionChangeTime = Time.time;
-           calcuateNewMovementVector();
+        if (Time.time > directionChangeTime * Random.value + latestDirectionChangeTime)
+        {
+            latestDirectionChangeTime = Time.time;
+            calcuateNewMovementVector();
         }
 
         //move enemy: 
-        transform.position = new Vector2(Mathf.Min(transform.position.x + (movementPerSecond.x * Time.deltaTime), width - spriteWidth),
-        Mathf.Min(transform.position.y + (movementPerSecond.y * Time.deltaTime), height - spriteHeight));
- 
+        //transform.position = new Vector2(Mathf.Min(transform.position.x + (movementPerSecond.x * Time.deltaTime), width - spriteWidth),
+        //Mathf.Min(transform.position.y + (movementPerSecond.y * Time.deltaTime), height - spriteHeight));
+        rb2d.velocity = movementPerSecond;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
     }
 }

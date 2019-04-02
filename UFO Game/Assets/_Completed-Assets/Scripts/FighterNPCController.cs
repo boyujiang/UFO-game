8dff9cf11626e4bf9109d5e327f3054abc8335d5
Tao Sophia <taos1@mcmaster.ca>
2019-04-01 21:03:03 -0400

    Add new tileset

---
+++ UFO Game/Assets/_Completed-Assets/Scripts/FighterNPCController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterNPCController : NPCController
+{
+    // Start is called before the first frame update
+    private readonly float projectileLatencyS = 1.2f;
+    private float timeSinceLastProjectile = 0f;
+    public GameObject projectile;
+    Vector2 projectilePos;
+
+    private void Start()
+    {
+        //projectile = Resources.Load("Projectile") as GameObject;
+        base.Start();
+    }
+
+    void CreateProjectile()
+    {
+        projectilePos = this.transform.position;
+        Instantiate(projectile, projectilePos, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - projectileLatencyS > timeSinceLastProjectile)
+        {
+            CreateProjectile();
+            timeSinceLastProjectile = Time.time;
+        }
+        base.Update();
+    }
+}
